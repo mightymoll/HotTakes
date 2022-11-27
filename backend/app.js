d46dd2de4da3b2833aa8268ece28a853f387ed95
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-11-27 12:33:51 +0100

    backend server setup
    - base backend for using with Node.js server
    - installed 'Express' middleware
    - connection to mongoDB
    
    P6 of OpenClassrooms web developer program

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use((req, res) => { req.json({ message: 'connected' }) })
+
+mongoose.connect('mongodb+srv://user123:admin123@cluster0.7kstqa6.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.get((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+module.exports = app;
