8fe215332e1d2cf94f72997f763043a1f0b150c8
mightymoll <109845481+mightymoll@users.noreply.github.com>
2022-11-28 20:30:47 +0100

    feat: new user signup
    - user email and password is stored in  mongo DB from 'signup' page
    - bcrypt password hash is working > db shows crypted string as password
    
    Errors:
     - problem with CORS (says no header present, but they are added in app.js)
    - POST request listed as 'failed' err 500
    
    P6 for OpenClassrooms web developer program

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 
-app.use((req, res) => { req.json({ message: 'connected' }) })
+// conenct app with Express
+app.use(express.json());
 
-mongoose.connect('mongodb+srv://user123:admin123@cluster0.7kstqa6.mongodb.net/?retryWrites=true&w=majority',
+// Express routes
+app.use('/api/auth', userRoutes);
+
+// connect to mongo DB with databaseURL from .env file
+mongoose.connect(process.env.databaseURL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log('Sucess! Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Fail! Connexion à MongoDB échouée !' + error));
 
-app.get((req, res, next) => {
+// set headers for CORS
+userRoutes.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.status(200).send('OK')
   next();
 });
 
